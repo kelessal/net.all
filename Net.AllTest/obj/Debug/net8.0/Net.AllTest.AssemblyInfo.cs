a827255a7a626f47ec8fa27e12567c7c45d5b0be
Salih <keles.salih@gmail.com>
2024-01-14 08:29:59 +0300

    init

--- Net.AllTest/obj/Debug/net8.0/Net.AllTest.AssemblyInfo.cs
+++ Net.AllTest/obj/Debug/net8.0/Net.AllTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Net.AllTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5961dcafc4a01b2ffc09468a75dc95965f5f617f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Net.AllTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Net.AllTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
