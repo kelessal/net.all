5961dcafc4a01b2ffc09468a75dc95965f5f617f
Salih <keles.salih@gmail.com>
2024-01-14 08:19:29 +0300

    initial

---
+++ Net.All/Proxy/Attributes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net.Proxy
+{
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple =false)]
+    public class NoTrackDataAttribute:Attribute
+    {
+    }
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple =false)]
+    public class StrictCompareDataAttribute : Attribute
+    {
+      
+    }
+}
---
+++ Net.AllTest/Json/BigObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net.All31Test.Json
+{
+    public class BigObject
+    {
+        public string Property1 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property2 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property3 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property4 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property5 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property6 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property7 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property8 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property9 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property10 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property11 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property12 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property13 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property14 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property15 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property16 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property17 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property18 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+        public string Property19 { get; set; } = "Shows the license plate information of the guest's vehicle or vehicles.";
+
+
+    }
+}
